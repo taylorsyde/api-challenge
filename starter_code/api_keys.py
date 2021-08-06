ccf44f039cb2a7269549e0859ae161c92a947681
taylorsyde <tay.lyons@hotmail.com>
2021-08-06 16:15:50 -0500

    verify gitignore

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "1d73b8f870b0049d086762bef2afd46c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAKesFp-qAcKAQybALskCiu6E3XskbRWuo"
